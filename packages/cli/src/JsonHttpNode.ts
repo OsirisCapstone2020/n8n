ef409a89610571efef6b47e2d3c186b3c3d6e646
Dumilk <dariusdum@gmail.com>
2021-03-15 18:54:38 -0700

    updated url for each json node

--- packages/cli/src/JsonHttpNode.ts
+++ packages/cli/src/JsonHttpNode.ts
@@ -11,25 +11,25 @@ export class JsonHttpNode implements INodeType {
 
 	async executeSingle(this: IExecuteSingleFunctions): Promise<INodeExecutionData> {
 		console.log("GOT HERE");
-		// const qParams = new URLSearchParams();
-		// const inputData = { ...this.getInputData() };
-		// const url = this.getNodeParameter("url") as string;
-		//
-		// console.log(JSON.stringify(node));
+		const qParams = new URLSearchParams();
+		const inputData = { ...this.getInputData() };
+		const url = this.getNodeParameter("url") as string;
+		
+		console.log(JSON.stringify(node));
 
-		// Object.keys(node.parameters).forEach((qKey) => {
-		// 	qParams.append(qKey, JSON.stringify(this.getNodeParameter(qKey)));
-		// });
+		Object.keys(node.parameters).forEach((qKey) => {
+			qParams.append(qKey, JSON.stringify(this.getNodeParameter(qKey)));
+		});
 
-		// const response: IDataObject = await this.helpers.request({
-		// 	url: url,
-		// 	qs: qParams.toString(),
-		// 	method: "POST",
-		// 	json: true,
-		// 	body: inputData.json,
-		// });
-		//
-		// return Object.assign({}, inputData, { json: response });
+		const response: IDataObject = await this.helpers.request({
+			url: url,
+			qs: qParams.toString(),
+			method: "POST",
+			json: true,
+			body: inputData.json,
+		});
+		
+		return Object.assign({}, inputData, { json: response });
 
 		return Object.assign({}, this.getInputData());
 	}
